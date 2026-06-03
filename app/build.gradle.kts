71221a55c38a9fd6203cf891b0df2df38de311ca
yoanosiel <151474332+yoanosiel@users.noreply.github.com>
2026-06-03 17:11:30 +0000

    Solución: Bloque de firmas obsoleto comentado correctamente en Kotlin

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,31 +20,31 @@ android {
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
 
-//   signingConfigs {
-    create("release") {
-      val keystorePath = System.getenv("KEYSTORE_PATH") ?: "${rootDir}/my-upload-key.jks"
-      storeFile = file(keystorePath)
-      storePassword = System.getenv("STORE_PASSWORD")
-      keyAlias = "upload"
-      keyPassword = System.getenv("KEY_PASSWORD")
-    }
-    create("debugConfig") {
-      storeFile = file("${rootDir}/debug.keystore")
-      storePassword = "android"
-      keyAlias = "androiddebugkey"
-      keyPassword = "android"
-    }
-  }
+// //   signingConfigs {
+//     create("release") {
+//       val keystorePath = System.getenv("KEYSTORE_PATH") ?: "${rootDir}/my-upload-key.jks"
+//       storeFile = file(keystorePath)
+//       storePassword = System.getenv("STORE_PASSWORD")
+//       keyAlias = "upload"
+//       keyPassword = System.getenv("KEY_PASSWORD")
+//     }
+//     create("debugConfig") {
+//       storeFile = file("${rootDir}/debug.keystore")
+//       storePassword = "android"
+//       keyAlias = "androiddebugkey"
+//       keyPassword = "android"
+//     }
+//   }
 
   buildTypes {
     release {
       isCrunchPngs = false
       isMinifyEnabled = false
       proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
-//       signingConfig = signingConfigs.getByName("release")
+// //       signingConfig = signingConfigs.getByName("release")
     }
     debug {
-//       signingConfig = signingConfigs.getByName("debugConfig")
+// //       signingConfig = signingConfigs.getByName("debugConfig")
     }
   }
   compileOptions {
