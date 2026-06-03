97b1141d48ae6c4497aef97ba324386c408c8fbd
yoanosiel <151474332+yoanosiel@users.noreply.github.com>
2026-06-03 16:29:55 +0000

    Estructura CubAlquila completa: Compilador listo e iconos corregidos

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,11 +7,11 @@ plugins {
 }
 
 android {
-  namespace = "com.example"
+  namespace = "com.consciouspay.cubalquila"
   compileSdk { version = release(36) { minorApiLevel = 1 } }
 
   defaultConfig {
-    applicationId = "com.aistudio.alquilerescuba.krtnvh"
+    applicationId = "com.consciouspay.cubalquila"
     minSdk = 24
     targetSdk = 36
     versionCode = 1
