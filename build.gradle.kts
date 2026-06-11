de01200df4189035a10886c9eb8a5b3ee9cf43d5
yoanosiel <151474332+yoanosiel@users.noreply.github.com>
2026-06-11 22:04:04 +0000

    Corregida declaracion de plugins de Google Services

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
+    id("com.google.gms.google-services") version "4.4.1" apply false
   alias(libs.plugins.android.application) apply false
   alias(libs.plugins.kotlin.compose) apply false
   alias(libs.plugins.google.devtools.ksp) apply false
