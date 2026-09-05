2850960ba52e2e2ff8deb2ec410a577b69236e8f
yoanosiel <151474332+yoanosiel@users.noreply.github.com>
2026-09-06 01:15:52 +0200

    Remove remaining Parse initialization

--- app/src/main/java/com/example/CubAlquilaApplication.kt
+++ app/src/main/java/com/example/CubAlquilaApplication.kt
@@ -1,21 +1,9 @@
 package com.example
 
 import android.app.Application
-import com.parse.Parse
 
 class CubAlquilaApplication : Application() {
     override fun onCreate() {
         super.onCreate()
-
-        val appId = "M93BK0NLWqLy32CvT5fwVceB12mLYurgrh5L2VKb"
-        val clientKey = "N0XwhiJ6a0HFibrtWEQOtrTopbCbE0UwZPFpHBZB"
-
-        Parse.initialize(
-            Parse.Configuration.Builder(this)
-                .applicationId(appId)
-                .clientKey(clientKey)
-                .server("https://silent-sun-3c53.osiel-garcia9803.workers.dev")
-                .build()
-        )
     }
 }
